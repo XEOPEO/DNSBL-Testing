d4691b23364991ddb9930b6a77a8ba515614494b
Cedric Dellisse <3829939+XEOPEO@users.noreply.github.com>
2022-09-07 11:14:28 +0200

    dnsbl-test.sh script file

---
+++ dnsbl-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# File to read domains from
+blocklist_file="<dns-bl-filename>"
+
+blockedcount=0
+percentage=0
+testedcount=0
+
+for domain in $(cat $blocklist_file)
+do
+  let testedcount++
+  echo "Domain: ${domain}"
+  ip=$(dig +short $domain | tail -n1)
+  if [ -z "$ip" ]
+  then
+    let blockedcount++
+    echo "Blocked"
+    echo "${blockedcount}/${testedcount}"
+  else
+    echo "${blockedcount}/${testedcount}"
+  fi
+  blockrate=$((100*blockedcount/testedcount))
+  echo "Blockrate: ${blockrate}%"
+  #sleep 0.05
+done
+
+let percentage=100*blockedcount/testedcount
+
+echo "The DNS Server blocked ${blockedcount} domains"
+echo "This is a ${percentage}% block rate"
