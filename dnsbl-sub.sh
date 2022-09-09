3f0ccac01805a2e23301bed1655894b5b28660b7
Cedric Dellisse <3829939+XEOPEO@users.noreply.github.com>
2022-09-09 14:51:03 +0200

    Update dnsbl-sub.sh

--- dnsbl-sub.sh
+++ dnsbl-sub.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 # File to read domains from
 blocklist_file="wordfiles/$1"
+dnsip="$2"
 
 # Count total domains in blocklist
 blockedcount=0
-percentage=0
 testedcount=0
 
 for domain in $(cat $blocklist_file)
 do
   let testedcount++
-  ip=$(dig +short $domain | tail -n1)
-  if [ -z "$ip" ]
+  ip=$(dig @$dnsip +short $domain | tail -n1)
+  if [ -z "$ip" ] || [ "$ip" == "0.0.0.0" ] || [ "$ip" == "198.251.90.71" ] || [ "$ip" == "103.224.182.210" ]
   then
     let blockedcount++
   fi
+  echo -e "${blockedcount}\n${testedcount}" > "results/result-$1.txt"
   sleep 0.2
 done
-
-echo "${blockedcount}"
