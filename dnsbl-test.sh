6fa5f7d4a2c7a604c5ece10bad9f9fa3d80b6826
Cedric Dellisse <3829939+XEOPEO@users.noreply.github.com>
2022-09-21 16:40:05 +0200

    Update dnsbl-test.sh

--- dnsbl-test.sh
+++ dnsbl-test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # File to read domains from
-blocklist_file="<dns_blocklist_file>"
+read -p "Blocklist name to use: " blocklist_file
+
+# Count total domains in blocklist
+#rowcount_blocklist=$(cat stevenblackbl | wc -l)
+read -p "DNS ServerIP: " dnsip
 
 blockedcount=0
 percentage=0
@@ -9,19 +13,14 @@ testedcount=0
 for domain in $(cat $blocklist_file)
 do
   let testedcount++
-  echo "Domain: ${domain}"
-  ip=$(dig +short $domain | tail -n1)
-  if [ -z "$ip" ]
-  then
+  echo "Domain: $domain"
+  ip=$(dig @$dnsip +short $domain | tail -n1)
+  if [ -z "$ip" ] || [ "$ip" == "0.0.0.0" ] || [ "$ip" == "198.251.90.71" ] || [ "$ip" == "103.224.182.210" ] || [ "$ip" == "2.2.2.2" ] || [ "$ip" == "52.212.6.220" ] || [ "$ip" == "52.17.217.207" ]; then
     let blockedcount++
-    echo "Blocked"
-    echo "${blockedcount}/${testedcount}"
-  else
-    echo "${blockedcount}/${testedcount}"
   fi
   blockrate=$((100*blockedcount/testedcount))
-  echo "Blockrate: ${blockrate}%"
-  #sleep 0.05
+  echo $ip
+  sleep 0.2
 done
 
 let percentage=100*blockedcount/testedcount
